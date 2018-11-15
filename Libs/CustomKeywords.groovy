286f3a81d8e15b89fcb03a9d0c98e81c5fdc1f4d
Nazneen Fatma <39731208+nazneen-dapl@users.noreply.github.com>
2018-11-15 18:52:37 +0530

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,35 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+def static "clickEvent.SelectServices.refreshBrowser"() {
+    (new clickEvent.SelectServices()).refreshBrowser()
+}
+
+def static "clickEvent.SelectServices.clickElement"(
+    	TestObject to	) {
+    (new clickEvent.SelectServices()).clickElement(
+        	to)
+}
+
+def static "clickEvent.SelectServices.getHtmlTableRows"(
+    	TestObject table	
+     , 	String outerTagName	) {
+    (new clickEvent.SelectServices()).getHtmlTableRows(
+        	table
+         , 	outerTagName)
+}
+
+def static "clickEvent.SelectServices.clickUsingJS1"(
+    	TestObject to	
+     , 	int timeout	) {
+    (new clickEvent.SelectServices()).clickUsingJS1(
+        	to
+         , 	timeout)
+}
